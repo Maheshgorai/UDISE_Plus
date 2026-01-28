7ab84fe5a2ee602a668cb8ab1c81f239dd5c6797
Mahesh Gorai <maheshgorai202@gmail.com>
2026-01-28 21:09:26 +0530

    Update Enrollments_24_25_UDISE+.R

--- Enrollments_24_25_UDISE+.R
+++ Enrollments_24_25_UDISE+.R
@@ -5,7 +5,7 @@ State <- read_csv("D:/Users/Downloads/profile_data_1_All State_2024-25 (1)/100_p
 Students <- read_csv("D:/Users/Downloads/enrolment_data_2_All State_2024-25/100_enr2.csv")
 
 
-Students <- Students %>%
+Students <- Students 
   group_by(pseudocode) %>%
   summarise( 
     across(matches("^(cpp|c\\d{1,2})_(b|g)$"),
@@ -1992,6 +1992,7 @@ Combined_class12 <- list(
   "Combined_class12_Pri_Unaided" = private_unaided_c12_final,
   "Combined_class12_Overall" = overall_c12_final
 )
+
 
 
 
